f94ee2f7715e8ed11c1154cc3c6d93602f8c77bf
James Thurley <mail@jamesthurley.com>
2014-11-22 16:42:18 +0000

    Travis CI modification

--- .travis/post_build.sh
+++ .travis/post_build.sh
@@ -1,9 +1,16 @@
 
+git config --global user.email "services@fifthweek.com"
+git config --global user.name "TravisCI"
+
 mkdir $HOME/temp_dist
+
+echo Cloning fifthweek-web-dist
 git clone -b master git@github.com:fifthweek/fifthweek-web-dist.git $HOME/temp_dist
 
-rm -rf $HOME/temp_dist/* > /dev/null
+echo Clearing files
+rm -rf $HOME/temp_dist/*
 
+echo Copying new files
 cp -rf dist/* $HOME/temp_dist/
 
 # Use to troubleshoot render issues.
@@ -13,8 +20,11 @@ cp -rf dist/* $HOME/temp_dist/
 # mkdir $HOME/temp_dist/tmp
 # cp -rf test/screenshots $HOME/temp_dist/tmp/
 
+echo Changing directory
 cd $HOME/temp_dist
+pwd
 
+echo Pushing new files to fifthweek-web-dist
 git add -A
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER from fithweek-web"
 git push git@github.com:fifthweek/fifthweek-web-dist.git master
