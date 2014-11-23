dc367877af2b2549ec07ccd903888dbf2398b762
James Thurley <mail@jamesthurley.com>
2014-11-23 18:46:46 +0000

    Deployment change

--- .travis/pre_build.sh
+++ .travis/pre_build.sh
@@ -1,3 +1,9 @@
 
 echo Replacing API uri
 replace "http://localhost:9000" "http://fifthweek-api.azurewebsites.net" -- app/scripts/app.js
+
+echo Browser Stack Username:
+echo $BROWSER_STACK_USERNAME
+
+echo Browser Stack Access Key:
+echo $BROWSER_STACK_ACCESS_KEY
