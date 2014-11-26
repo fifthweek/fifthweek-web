1ed608d154dbb4e29aedb1abf30539a6eb29f509
James Thurley <mail@jamesthurley.com>
2014-11-26 11:50:43 +0000

    Reduced protractor sessions to 1. Improved Sign-in UI.

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -9,7 +9,7 @@ exports.config = {
   seleniumAddress: 'http://hub.browserstack.com/wd/hub',
   baseUrl: 'http://localhost:9001',
   allScriptsTimeout: 55000,
-  maxSessions: 2,
+  maxSessions: 1,
   // Capabilities to be passed to the webdriver instance.
 
   multiCapabilities: [{
