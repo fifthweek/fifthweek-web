a11a85abcb389642ea1c41417f79eaf946b4ca0f
Lawrence Wagerfield <lawrence@wagerfield.com>
2014-12-08 16:22:39 +0000

    Add validation to username

---
+++ app/scripts/directives/username.js
@@ -0,0 +1,31 @@
+angular.module('webApp').directive('username', function($q, $timeout) {
+  'use strict';
+
+  return {
+    require: 'ngModel',
+    link: function(scope, elm, attrs, ctrl) {
+      ctrl.$asyncValidators.username = function(modelValue) {
+
+        if (ctrl.$isEmpty(modelValue)) {
+          // consider empty model valid
+          return $q.when();
+        }
+
+        var def = $q.defer();
+
+        $timeout(function() {
+          // Mock a delayed response
+          //if (usernames.indexOf(modelValue) === -1) {
+            // The username is available
+            def.resolve();
+          //} else {
+          //  def.reject();
+          //}
+
+        }, 1000);
+
+        return def.promise;
+      };
+    }
+  };
+});
--- test/e2e/fifthweek.spec.js
+++ test/e2e/fifthweek.spec.js
@@ -1,16 +1,6 @@
 describe('fifthweek', function() {
   'use strict';
 
-  var username = 'webdriver_' + Date.now();
-
-  var signInLink = element(by.id('signInLink'));
-  var registerLink = element(by.id('registerLink'));
-
-
-  beforeEach(function() {
-    browser.get('/');
-  });
-
   //it('should have a sign in link', function() {
   //  expect(signInLink.getText()).toContain('Sign In');
   //});
@@ -20,15 +10,6 @@ describe('fifthweek', function() {
   });
 
   describe('register page', function() {
-    var exampleWorkTextBox = element(by.model('registrationData.exampleWork'));
-    var emailTextBox = element(by.model('registrationData.email'));
-    var usernameTextBox = element(by.model('registrationData.username'));
-    var passwordTextBox = element(by.model('registrationData.password'));
-    var registerButton = element(by.id('registerButton'));
-
-    beforeEach(function() {
-      registerLink.click();
-    });
 
     it('should allow a new user to register', function(){
       exampleWorkTextBox.sendKeys(username);
@@ -70,5 +51,79 @@ describe('fifthweek', function() {
       registerButton.click();
       // Todo: assert failure
     });
+
+    it('should allow lowercase and uppercase characters in username', function(){
+      exampleWorkTextBox.sendKeys(username);
+      emailTextBox.sendKeys(username + '@mailinator.com');
+      usernameTextBox.sendKeys("abcABC");
+      passwordTextBox.sendKeys('password1');
+      registerButton.click();
+      // Todo: assert check for success
+    });
+
+    it('should allow numbers in username', function(){
+      exampleWorkTextBox.sendKeys(username);
+      emailTextBox.sendKeys(username + '@mailinator.com');
+      usernameTextBox.sendKeys("123");
+      passwordTextBox.sendKeys('password1');
+      registerButton.click();
+      // Todo: assert check for success
+    });
+
+    it('should allow hypens and underscores in username', function(){
+      exampleWorkTextBox.sendKeys(username);
+      emailTextBox.sendKeys(username + '@mailinator.com');
+      usernameTextBox.sendKeys("abc_ABC-123");
+      passwordTextBox.sendKeys('password1');
+      registerButton.click();
+      // Todo: assert check for success
+    });
+
+    it('should allow leading and trailing spaces in username', function(){
+      exampleWorkTextBox.sendKeys(username);
+      emailTextBox.sendKeys(username + '@mailinator.com');
+      usernameTextBox.sendKeys(" abc ");
+      passwordTextBox.sendKeys('password1');
+      registerButton.click();
+      // Todo: assert check for success
+    });
+
+    it('should not allow spaces in username', function(){
+      exampleWorkTextBox.sendKeys(username);
+      emailTextBox.sendKeys(username + '@mailinator.com');
+      usernameTextBox.sendKeys("abc ABC");
+      passwordTextBox.sendKeys('password1');
+      registerButton.click();
+      // Todo: assert check for failure
+    });
+
+    it('should not allow forbidden characters in username', function(){
+      exampleWorkTextBox.sendKeys(username);
+      emailTextBox.sendKeys(username + '@mailinator.com');
+      usernameTextBox.sendKeys("abc!");
+      passwordTextBox.sendKeys('password1');
+      registerButton.click();
+      // Todo: assert check for failure
+      // Todo: Repeat with several different characters...
+    });
+
+    var exampleWorkTextBox = element(by.model('registrationData.exampleWork'));
+    var emailTextBox = element(by.model('registrationData.email'));
+    var usernameTextBox = element(by.model('registrationData.username'));
+    var passwordTextBox = element(by.model('registrationData.password'));
+    var registerButton = element(by.id('registerButton'));
+
+    beforeEach(function() {
+      registerLink.click();
+    });
+  });
+
+  var username = 'webdriver_' + Date.now();
+
+  var signInLink = element(by.id('signInLink'));
+  var registerLink = element(by.id('registerLink'));
+
+  beforeEach(function() {
+    browser.get('/');
   });
 });
