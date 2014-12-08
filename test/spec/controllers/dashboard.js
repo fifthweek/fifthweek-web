00b7365c17e3d8fc7b4ead96f4846fb13e3dfc71
Lawrence Wagerfield <lawrence@wagerfield.com>
2014-12-08 14:29:14 +0000

    Add copy to dashboard

---
+++ test/spec/controllers/dashboard.js
@@ -0,0 +1,19 @@
+'use strict';
+
+describe('dashboard controller', function () {
+
+  // load the controller's module
+  beforeEach(module('webApp'));
+
+  var DashboardCtrl,
+    scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    DashboardCtrl = $controller('DashboardCtrl', {
+      $scope: scope
+    });
+  }));
+
+});
