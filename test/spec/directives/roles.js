e2f138681f56940ce0a52f80811ced83666bff99
Lawrence Wagerfield <lawrence@wagerfield.com>
2014-12-08 12:59:07 +0000

    Update tests to a more natural language. Move boilerplate to bottom of tests.

--- test/spec/directives/roles.js
+++ test/spec/directives/roles.js
@@ -1,5 +1,100 @@
-describe('Directive: roles', function(){
+describe('roles directive', function(){
   'use strict';
+
+  describe('when roles are passed in', function() {
+    it('should call authorize with specified roles and role check type', function(){
+      expect(authorizationService.authorize).toHaveBeenCalled();
+      expect(authorizationService.authorize.calls.argsFor(0)).toEqual([true, ['Test1', 'Test2'], 'all']);
+    });
+
+    var element;
+
+    beforeEach(function(){
+      element = angular.element('<button roles="Test1, Test2" role-check-type="all"/>');
+
+      authorizationService.authorize = function(){
+        return authorizationServiceConstants.authorizationResult.authorized;
+      };
+
+      spyOn(authorizationService, 'authorize').and.callThrough();
+
+      inject(function($rootScope, $compile){
+        var scope = $rootScope.$new();
+        scope.name = 'test';
+        $compile(element)(scope);
+        scope.$digest();
+      });
+    });
+  });
+
+  describe('when authorized', function() {
+    it('should display the button', function(){
+      expect(element.hasClass('hidden')).toBeFalsy();
+    });
+
+    var element;
+
+    beforeEach(function(){
+      element = angular.element('<button roles="" role-check-type=""/>');
+
+      authorizationService.authorize = function() {
+        return authorizationServiceConstants.authorizationResult.authorized;
+      };
+
+      inject(function($rootScope, $compile){
+        var scope = $rootScope.$new();
+        scope.name = 'test';
+        $compile(element)(scope);
+        scope.$digest();
+      });
+    });
+  });
+
+  describe('when not authorized', function() {
+    it('should not display the button', function(){
+      expect(element.hasClass('hidden')).toBeTruthy();
+    });
+
+    var element;
+
+    beforeEach(function(){
+      element = angular.element('<button roles="" role-check-type=""/>');
+
+      authorizationService.authorize = function() {
+        return authorizationServiceConstants.authorizationResult.notAuthorized;
+      };
+
+      inject(function($rootScope, $compile){
+        var scope = $rootScope.$new();
+        scope.name = 'test';
+        $compile(element)(scope);
+        scope.$digest();
+      });
+    });
+  });
+
+  describe('when login required', function() {
+    it('should not display the button', function(){
+      expect(element.hasClass('hidden')).toBeTruthy();
+    });
+
+    var element;
+
+    beforeEach(function(){
+      element = angular.element('<button roles="" role-check-type=""/>');
+
+      authorizationService.authorize = function() {
+        return authorizationServiceConstants.authorizationResult.loginRequired;
+      };
+
+      inject(function($rootScope, $compile){
+        var scope = $rootScope.$new();
+        scope.name = 'test';
+        $compile(element)(scope);
+        scope.$digest();
+      });
+    });
+  });
 
   beforeEach(function() {
     module('webApp');
@@ -19,103 +114,4 @@ describe('Directive: roles', function(){
   beforeEach(inject(function($injector) {
     authorizationServiceConstants = $injector.get('authorizationServiceConstants');
   }));
-
-  describe('when roles are passed in', function()
-  {
-    var element;
-
-    beforeEach(function(){
-      element = angular.element('<button roles="Test1, Test2" role-check-type="all"/>');
-
-      authorizationService.authorize = function(){
-        return authorizationServiceConstants.authorizationResult.authorized;
-      };
-
-      spyOn(authorizationService, 'authorize').and.callThrough();
-
-      inject(function($rootScope, $compile){
-        var scope = $rootScope.$new();
-        scope.name = 'test';
-        $compile(element)(scope);
-        scope.$digest();
-      });
-    });
-
-    it('should call authorize with specified roles and role check type', function(){
-      expect(authorizationService.authorize).toHaveBeenCalled();
-      expect(authorizationService.authorize.calls.argsFor(0)).toEqual([true, ['Test1', 'Test2'], 'all']);
-    });
-  });
-
-  describe('when authorized', function()
-  {
-    var element;
-
-    beforeEach(function(){
-      element = angular.element('<button roles="" role-check-type=""/>');
-
-      authorizationService.authorize = function() {
-        return authorizationServiceConstants.authorizationResult.authorized;
-      };
-
-      inject(function($rootScope, $compile){
-        var scope = $rootScope.$new();
-        scope.name = 'test';
-        $compile(element)(scope);
-        scope.$digest();
-      });
-    });
-
-    it('should display the button', function(){
-      expect(element.hasClass('hidden')).toBeFalsy();
-    });
-  });
-
-  describe('when not authorized', function()
-  {
-    var element;
-
-    beforeEach(function(){
-      element = angular.element('<button roles="" role-check-type=""/>');
-
-      authorizationService.authorize = function() {
-        return authorizationServiceConstants.authorizationResult.notAuthorized;
-      };
-
-      inject(function($rootScope, $compile){
-        var scope = $rootScope.$new();
-        scope.name = 'test';
-        $compile(element)(scope);
-        scope.$digest();
-      });
-    });
-
-    it('should not display the button', function(){
-      expect(element.hasClass('hidden')).toBeTruthy();
-    });
-  });
-
-  describe('when login required', function()
-  {
-    var element;
-
-    beforeEach(function(){
-      element = angular.element('<button roles="" role-check-type=""/>');
-
-      authorizationService.authorize = function() {
-        return authorizationServiceConstants.authorizationResult.loginRequired;
-      };
-
-      inject(function($rootScope, $compile){
-        var scope = $rootScope.$new();
-        scope.name = 'test';
-        $compile(element)(scope);
-        scope.$digest();
-      });
-    });
-
-    it('should not display the button', function(){
-      expect(element.hasClass('hidden')).toBeTruthy();
-    });
-  });
 });
