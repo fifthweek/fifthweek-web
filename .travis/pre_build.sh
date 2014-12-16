801a2d260839a1222d5c8f80bed2517580324aa0
James Thurley <mail@jamesthurley.com>
2014-12-16 10:39:37 +0000

    Move text replacement from travis task to grunt task. Add New Relic javascript for live site.

--- .travis/pre_build.sh
+++ .travis/pre_build.sh
@@ -1,9 +1,3 @@
 
-echo Replacing API uri
-replace "https://10.211.55.3:44301" "https://api.fifthweek.com" -- app/scripts/app.js
-
 echo Browser Stack Username:
 echo $BROWSER_STACK_USERNAME
-
-echo Browser Stack Access Key:
-echo $BROWSER_STACK_ACCESS_KEY
