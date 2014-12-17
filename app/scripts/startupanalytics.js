a4b9332c6070b5c41b21715749ac92e128ec9b2b
Lawrence Wagerfield <lawrence@wagerfield.com>
2014-12-17 19:15:11 +0000

    Add support for GA for provenance events

--- app/scripts/startupanalytics.js
+++ app/scripts/startupanalytics.js
@@ -8,11 +8,32 @@ angular.module('webApp').run(function($analytics) {
     return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
   }
 
+  // Angulartics mutates input parameters which causes events to bleed into each other, resulting in erroneous tracking.
+  var eventCategory = function() {
+    return {category: 'Provenance'};
+  };
+
   var emailAddress = getParameterByName('emailed_to');
   if (emailAddress.length > 0) {
     $analytics.setUserProperties({
       'last opened email from': emailAddress
     });
+    $analytics.eventTrack('Arrived from email to "' + emailAddress + '"', eventCategory());
+  }
+
+  var utmCampaign = getParameterByName('utm_campaign');
+  var utmSource = getParameterByName('utm_source');
+  var utmMedium = getParameterByName('utm_medium');
+  var utmContent = getParameterByName('utm_content');
+
+  if (utmCampaign.length > 0 || utmSource.length > 0 || utmMedium.length > 0 || utmContent.length > 0) {
+    $analytics.eventTrack('Arrived from campaign "' +
+      utmCampaign + '/' +
+      utmSource + '/' +
+      utmMedium + '/' +
+      utmContent + '/' +
+      '"',
+      eventCategory());
   }
 });
 
