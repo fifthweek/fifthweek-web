132a657b44c62a755eecd5c435e3f812f8b76379
Lawrence Wagerfield <lawrence@wagerfield.com>
2014-12-17 20:12:45 +0000

    Remove trailing forward-slash from tracking data on UTM

--- app/scripts/startupanalytics.js
+++ app/scripts/startupanalytics.js
@@ -35,8 +35,7 @@ angular.module('webApp').run(function($analytics) {
       utmCampaign + '/' +
       utmSource + '/' +
       utmMedium + '/' +
-      utmContent + '/' +
-      '"',
+      utmContent + '"',
       eventCategory());
   }
 });
