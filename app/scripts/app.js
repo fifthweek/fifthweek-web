0cfc33e69f77e981e78ade3c9f1234e140547bd3
ttbarnes <tony@tonybarnes.me>
2014-12-23 09:40:31 +0000

    tidyup after rebase

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -129,32 +129,6 @@ angular
         redirectTo: fifthweekConstants.homePage  // TODO: Create a "Not Authorized" page.
       });
 
-    /*
-    .state('state1', {
-      url: '/state1',
-      templateUrl: 'views/state1.html'
-    })
-    .state('state1.list', {
-      url: '/list1',
-      templateUrl: 'views/state1.list.html',
-      controller: function($scope) {
-        $scope.items = ['A', 'List', 'Of', 'Items'];
-      }
-    })
-    .state('state2', {
-      url: '/state2',
-      templateUrl: 'views/state2.html'
-    })
-    .state('state2.list', {
-      url: '/list2',
-      templateUrl: 'views/state2.list.html',
-      controller: function($scope) {
-        $scope.things = ['A', 'Set', 'Of', 'Things'];
-      }
-    });
-});
-    */
-
     snapRemoteProvider. globalOptions = {
       disable: 'right',
       touchToDrag: false
