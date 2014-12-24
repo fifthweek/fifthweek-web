ea1051151e34cf69829446a4255870286b04f416
ttbarnes <tony@tonybarnes.me>
2014-12-24 11:07:05 +0000

    sign out page title

--- app/scripts/controllers/signout.js
+++ app/scripts/controllers/signout.js
@@ -1,7 +1,14 @@
 angular.module('webApp')
   .controller('SignOutCtrl', ['$location', 'authenticationService', 'fifthweekConstants',
-  	function($location, authenticationService, fifthweekConstants) {
+  	function($rootScope, $location, authenticationService, fifthweekConstants) {
 		'use strict';
+
+      //temporary page title solution
+      //this should be coming from a service
+      var brandName = 'Fifthweek';
+      var pageTitle = 'Sign Out';
+      $rootScope.headTitle = brandName + ' - ' + pageTitle;
+
 			authenticationService.signOut();
 			$location.path(fifthweekConstants.signInPage);
   	}
