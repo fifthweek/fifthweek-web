6348f091201b41863c26abf795aedb46ff4591a3
ttbarnes <tony@tonybarnes.me>
2015-01-14 21:39:22 +0000

    register ctrl as home ctrl, move register form into home page

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -1,11 +1,49 @@
-angular.module('webApp').controller(
-  'HomeCtrl', function ($scope, $modal) {
-    'use strict';
+angular.module('webApp').controller('HomeCtrl',
+  function($scope, $location, $analytics, authenticationService, fifthweekConstants, logService, utilities) {
+  'use strict';
 
-    $scope.openModal = function(){
-      $modal.open({
-        templateUrl: 'views/home-modal.html'
-      });
+  if(authenticationService.currentUser.authenticated === true){
+    $location.path(fifthweekConstants.dashboardPage);
+  }
+
+  $scope.savedSuccessfully = false;
+  $scope.message = '';
+
+  $scope.registrationData = {
+    email: '',
+    username: '',
+    password: ''
+  };
+
+  $scope.register = function() {
+
+    var eventCategory = function() {
+      return {category: 'Registration'};
     };
 
-  });
+    var handleSubmissionError = function(errorMessage){
+      $analytics.eventTrack('Registration failed', eventCategory());
+      $scope.message = errorMessage;
+    };
+
+    $analytics.eventTrack('Registration submitted', eventCategory());
+
+    return authenticationService.registerUser($scope.registrationData).then(function() {
+      $scope.savedSuccessfully = true;
+      $scope.message = 'Signing in...';
+
+      var signInData = {
+        username: $scope.registrationData.username,
+        password: $scope.registrationData.password
+      };
+
+      return authenticationService.signIn(signInData).then(function() {
+        $analytics.eventTrack('Registration succeeded', eventCategory());
+        $location.path(fifthweekConstants.dashboardPage);
+      });
+    }).catch(function(error) {
+      handleSubmissionError(utilities.getFriendlyErrorMessage(error));
+      return logService.error(error);
+    });
+  };
+});
