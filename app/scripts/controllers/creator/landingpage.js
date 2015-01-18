4e028660b408031d918462fdb871b39c07a7b591
ttbarnes <tony@tonybarnes.me>
2015-01-18 11:43:12 +0000

    improve checkbox model data, price binding

--- app/scripts/controllers/creator/landingpage.js
+++ app/scripts/controllers/creator/landingpage.js
@@ -5,20 +5,29 @@ angular.module('webApp').controller(
 
     (function subscriptions()  {
 
+      $scope.totalPrice = '';
       $scope.subscriptions = {
-        basic:true,
-        extras:false
+        basic: {
+          checked:true,
+          price:'0.50'
+        },
+        extras: {
+          checked:false,
+          price:'0.75'
+        }
       }
 
-      $scope.$watch('subscriptions.basic', function() {
-        if($scope.subscriptions.basic === true) {
-          $scope.subscriptions.extras = false;
+      $scope.$watch('subscriptions.basic.checked', function() {
+        if($scope.subscriptions.basic.checked === true) {
+          $scope.subscriptions.extras.checked = false;
+          $scope.totalPrice = $scope.subscriptions.basic.price;
         }
       });
 
-      $scope.$watch('subscriptions.extras', function() {
-        if($scope.subscriptions.extras === true) {
-          $scope.subscriptions.basic = false;
+      $scope.$watch('subscriptions.extras.checked', function() {
+        if($scope.subscriptions.extras.checked === true) {
+          $scope.subscriptions.basic.checked = false;
+          $scope.totalPrice = $scope.subscriptions.extras.price;
         }
       });
 
