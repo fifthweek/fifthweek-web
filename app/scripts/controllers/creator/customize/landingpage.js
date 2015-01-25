1977b71c0684ea5dabc1a768aa21b5b86f9e2eb7
ttbarnes <tony@tonybarnes.me>
2015-01-25 22:12:12 +0000

    hook up landing page customisation form data with controller

--- app/scripts/controllers/creator/customize/landingpage.js
+++ app/scripts/controllers/creator/customize/landingpage.js
@@ -3,6 +3,16 @@ angular.module('webApp').controller(
   function($scope) {
     'use strict';
 
+    $scope.landingPageData = {
+      url:'https://www.fifthweek.com/marc-holmes',
+      subscriptionName:'',
+      tagline:'',
+      introduction:'',
+      headerImage:'images/creator/landing-page/header-default.jpg',
+      videoUrl:'',
+      subscriptionAbout:''
+    };
+
 
   }
 ]);
