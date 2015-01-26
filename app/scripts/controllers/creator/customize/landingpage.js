2bd23ae52b775cf916ef82972e4fe03bc52b6326
ttbarnes <tony@tonybarnes.me>
2015-01-26 23:17:59 +0000

    disable submit buttons on click

--- app/scripts/controllers/creator/customize/landingpage.js
+++ app/scripts/controllers/creator/customize/landingpage.js
@@ -3,6 +3,8 @@ angular.module('webApp').controller(
   function($scope) {
     'use strict';
 
+    $scope.isSubmitting = false;
+
     $scope.landingPageData = {
       url:'https://www.fifthweek.com/marc-holmes',
       subscriptionName:'',
@@ -11,6 +13,10 @@ angular.module('webApp').controller(
       headerImage:'images/creator/landing-page/header-default.jpg',
       videoUrl:'',
       subscriptionAbout:''
+    };
+
+    $scope.saveTheDataTempFunctionName = function() {
+      $scope.isSubmitting = true;
     };
 
   }
