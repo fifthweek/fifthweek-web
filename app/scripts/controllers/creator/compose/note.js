91ae761199432c3cd81bb82d75c4a603764b60a4
ttbarnes <tony@tonybarnes.me>
2015-02-09 20:45:51 +0000

    compose note select data, hookup submit button with fw-button-loading

--- app/scripts/controllers/creator/compose/note.js
+++ app/scripts/controllers/creator/compose/note.js
@@ -5,5 +5,24 @@ angular.module('webApp').controller(
 
     $scope.postLater === false;
 
+    $scope.sharePreference = [
+      {
+        name:'Share with everyone', 
+        value:'shareWithAll'
+      },
+      {
+        name:'Share with subscribers', 
+        value:'shareWithSubscribers'
+      }
+    ];
+
+    $scope.sharePreferenceInit = $scope.sharePreference[0];
+
+    $scope.isSubmitting = false;
+
+    $scope.postNow = function() {
+      $scope.isSubmitting = true;
+    };
+
   }
 ]);
