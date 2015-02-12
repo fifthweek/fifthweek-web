949aeafeb889fa6c68f9d7689e32d01db48cef0e
Lawrence Wagerfield <lawrence@wagerfield.com>
2015-02-12 18:29:28 +0000

    General cleanup

--- app/scripts/controllers/creator/landing-page.js
+++ app/scripts/controllers/creator/landing-page.js
@@ -17,40 +17,39 @@ angular.module('webApp').controller('landingPageCtrl', function($scope) {
       price:'9.25',
       checked:false
     }
-  }
+  };
 
   $scope.checkboxHandler = function(){
     $scope.channels.basic.checked = true;
   };
 
-
   $scope.$watch('channels', function() {
 
     //create a blank array to store prices
-    var arr = new Array();
+    var selectedChannels = new Array();
 
     //iterate through each object
     //if an object has a 'checked' property with true value/boolean
     //add the object's price value to the array
-    for (var a in $scope.channels) {
-      var checked = $scope.channels[a]['checked'];
-      var price = $scope.channels[a]['price'];
-      if (checked == true)  {
+    for (var channel in $scope.channels) {
+      var checked = $scope.channels[channel].checked;
+      var price = $scope.channels[channel].price;
+      if (checked === true)  {
         //console.info('Checked: ', a);
-        arr.push(price);
+        selectedChannels.push(price);
 
         var totalPrice = 0;
 
         //add up the array for a total price
-        var arrLength = arr.length;
+        var arrLength = selectedChannels.length;
         while (arrLength--) {
-          totalPrice += parseFloat(arr[arrLength]);
+          totalPrice += parseFloat(selectedChannels[arrLength]);
         }
       }
       //remove price from array if unchecked
-      else if (checked == false)  {
+      else if (checked === false)  {
         //console.info('unchecked: ', price);
-        arr.splice(a);
+        selectedChannels.splice(channel);
       }
     }
     totalPrice = totalPrice.toFixed(2);
