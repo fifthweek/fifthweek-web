4254ad66214365a9499cf007501f1c12d67d8de2
ttbarnes <tony@tonybarnes.me>
2015-02-14 19:43:24 +0000

    sortable backlog items

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,6 +14,7 @@ angular
     'ui.bootstrap',
     'snap',
     'monospaced.elastic',
+    'ui.sortable',
     'angulartics',
     'angulartics.google.analytics',
     'angulartics.google.analytics.userid',
