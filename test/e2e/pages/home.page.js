5b4cadb7c3fe5131cac62b8b0b726451c19cb0fd
Lawrence Wagerfield <lawrence@wagerfield.com>
2015-02-15 00:01:59 +0000

    Add e2e test for homepage video

--- test/e2e/pages/home.page.js
+++ test/e2e/pages/home.page.js
@@ -3,7 +3,9 @@
 var HomePage = function() {};
 
 HomePage.prototype = Object.create({}, {
-  signInLink: { get: function () { return element(by.id('sign-in-link')); }}
+  signInLink: { get: function () { return element(by.id('sign-in-link')); }},
+  playVideoLink: { get: function () { return element(by.id('play-video-link')); }},
+  videoIFrame: { get: function () { return element(by.id('home-modal-video')); }}
 });
 
 module.exports = HomePage;
--- test/e2e/spec/home.spec.js
+++ test/e2e/spec/home.spec.js
@@ -7,11 +7,18 @@ describe("homepage", function() {
   var homePage = new HomePage();
   var signOutPage = new SignOutPage();
 
-  signOutPage.signOutAndGoHome();
+  beforeEach(function() {
+    signOutPage.signOutAndGoHome();
+  });
 
   it('should have a sign-in link', function() {
     expect(homePage.signInLink.getText()).toContain('Sign In');
     homePage.signInLink.click();
     expect(browser.getCurrentUrl()).toContain('/sign-in');
+  });
+
+  it('should link to the "creator journey" video', function() {
+    homePage.playVideoLink.click();
+    expect(homePage.videoIFrame.getAttribute('src')).toContain('player.vimeo.com/video/114229222');
   });
 });
