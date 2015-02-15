10c9c0ea0971b4812c773e05054e9587af1a52c7
ttbarnes <tony@tonybarnes.me>
2015-02-15 00:56:33 +0000

    e2e tests for home page intro copy

--- test/e2e/pages/home.page.js
+++ test/e2e/pages/home.page.js
@@ -4,6 +4,8 @@ var HomePage = function() {};
 
 HomePage.prototype = Object.create({}, {
   signInLink: { get: function () { return element(by.id('sign-in-link')); }},
+  introHeading: { get: function () { return element(by.id('intro-heading')); }},
+  introSubHeading: { get: function () { return element(by.id('intro-sub-heading')); }},
   playVideoLink: { get: function () { return element(by.id('play-video-link')); }},
   videoIFrame: { get: function () { return element(by.id('home-modal-video')); }}
 });
