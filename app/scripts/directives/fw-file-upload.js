d0d6850004eeaf3184db7870738cd4f8aa150e9d
Lawrence Wagerfield <lawrence@wagerfield.com>
2015-02-15 20:15:20 +0000

    Initial styling for new image and file post

--- app/scripts/directives/fw-file-upload.js
+++ app/scripts/directives/fw-file-upload.js
@@ -2,6 +2,9 @@ angular.module('webApp').directive('fwFileUpload', function () {
   'use strict';
 
     return {
+      link: function(scope, elem, attrs){
+        scope.fileType = attrs.fileType;
+      },
       templateUrl:'views/partials/file-upload.html'
     };
 
