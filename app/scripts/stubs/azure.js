6fd3c30c89e46f7db85e2a294e14ec5e1035427c
James Thurley <mail@jamesthurley.com>
2015-02-16 17:27:51 +0000

    Add stubs for azure blob uploading

---
+++ app/scripts/stubs/azure.js
@@ -0,0 +1,41 @@
+angular.module('webApp').factory('azureBlobStub',
+  function($http, $q) {
+    'use strict';
+    var service = {};
+
+    service.putBlockBlob = function(urlWithSasToken, blockId, requestData) {
+      var uri = urlWithSasToken + '&comp=block&blockid=' + blockId;
+      var config = {
+        headers: {
+          'x-ms-blob-type': 'BlockBlob'
+        }
+      };
+
+      return $http.put(uri, requestData, config)
+        .catch(function(response){
+          return $q.reject(new AzureError('Failed to upload block blob.', response));
+        });
+    };
+
+    service.commitBlockList = function(urlWithSasToken, blockIds, contentType) {
+      var uri = urlWithSasToken + '&comp=blocklist';
+
+      var requestBody = '<?xml version="1.0" encoding="utf-8"?><BlockList>';
+      for (var i = 0; i < blockIds.length; i++) {
+        requestBody += '<Latest>' + blockIds[i] + '</Latest>';
+      }
+      requestBody += '</BlockList>';
+      var config = {
+        headers: {
+          'x-ms-blob-content-type': contentType
+        }
+      };
+
+      return $http.put(uri, requestBody, config)
+        .catch(function(response){
+          return $q.reject(new AzureError('Failed to commit blob list.', response));
+        });
+    };
+
+    return service;
+  });
---
+++ test/spec/stubs/azure.js
@@ -0,0 +1,94 @@
+describe('azure stubs', function(){
+  'use strict';
+  
+  var target;
+  var $httpBackend;
+
+  beforeEach(function(){
+
+    module('webApp');
+
+    inject(function($injector) {
+      target = $injector.get('azureBlobStub');
+      $httpBackend = $injector.get('$httpBackend');
+    });
+  });
+
+  afterEach(function() {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
+  describe('when calling putBlockBlob', function(){
+
+    var url;
+    var blockId;
+    var requestData;
+
+    beforeEach(function(){
+      url = 'blah.com/?abc';
+      blockId = 'blockId';
+      requestData = { test: 1 };
+    });
+
+    it('should put the data to azure', function(){
+      $httpBackend.expectPUT(url + '&comp=block&blockid=' + blockId, requestData).respond(200);
+
+      var error;
+      target.putBlockBlob(url, blockId, requestData).catch(function(e) { error = e; });
+
+      $httpBackend.flush();
+
+      expect(error).toBeUndefined();
+    });
+
+    it('should return an AzureError if the call fails', function(){
+      $httpBackend.expectPUT(url + '&comp=block&blockid=' + blockId, requestData).respond(500);
+
+      var error;
+      target.putBlockBlob(url, blockId, requestData).catch(function(e) { error = e; });
+
+      $httpBackend.flush();
+
+      expect(error).toBeDefined();
+      expect(error instanceof AzureError).toBeTruthy();
+    });
+  });
+
+  describe('when calling commitBlobList', function(){
+
+    var url;
+    var blockIds;
+    var contentType;
+
+    beforeEach(function(){
+      url = 'blah.com/?abc';
+      blockIds = [ 'one', 'two', 'three' ];
+      contentType = 'image/jpeg';
+    });
+
+    it('should put the data to azure', function(){
+      var expectedData = '<?xml version="1.0" encoding="utf-8"?><BlockList><Latest>one</Latest><Latest>two</Latest><Latest>three</Latest></BlockList>';
+      $httpBackend.expectPUT(url + '&comp=blocklist', expectedData).respond(200);
+
+      var error;
+      target.commitBlockList(url, blockIds, contentType).catch(function(e) { error = e; });
+
+      $httpBackend.flush();
+
+      expect(error).toBeUndefined();
+    });
+
+    it('should return an AzureError if the call fails', function(){
+      $httpBackend.expectPUT(url + '&comp=blocklist').respond(500);
+
+      var error;
+      target.commitBlockList(url, blockIds, contentType).catch(function(e) { error = e; });
+
+      $httpBackend.flush();
+
+      expect(error).toBeDefined();
+      expect(error instanceof AzureError).toBeTruthy();
+    });
+  });
+});
