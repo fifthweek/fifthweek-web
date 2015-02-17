a7e68b8ff88671775564dead18d5ef9d92bb817a
Lawrence Wagerfield <lawrence@wagerfield.com>
2015-02-17 01:13:04 +0000

    Add binding to date picker for disabled state

--- app/scripts/directives/fw-datepicker.js
+++ app/scripts/directives/fw-datepicker.js
@@ -2,6 +2,9 @@ angular.module('webApp').directive('fwDatepicker', function () {
   'use strict';
 
     return {
+      scope: {
+        ngDisabled: '='
+      },
       templateUrl:'views/partials/datepicker.html'
     };
 
