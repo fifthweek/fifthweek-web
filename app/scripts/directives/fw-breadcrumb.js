6a80cf151c26892b208cbb0f45b05eafb72546c4
Lawrence Wagerfield <lawrence@wagerfield.com>
2015-02-21 16:32:38 +0000

    Generate breadcrumb from navigation orchestrator

--- app/scripts/directives/fw-breadcrumb.js
+++ app/scripts/directives/fw-breadcrumb.js
@@ -1,14 +1,24 @@
-angular.module('webApp').directive('fwBreadcrumb', function () {
+angular.module('webApp').directive('fwBreadcrumb', function (navigationOrchestrator) {
   'use strict';
 
   return {
     restrict: 'E',
     scope: {
-      itemSource: '&items'
+      intermediateStates: '=',
+      current: '='
     },
     templateUrl: 'views/partials/breadcrumb.html',
-    link:function (scope) {
-      scope.items = scope.itemSource();
+    link: {
+      pre: function (scope) {
+        var rootNavigationItem = _.find(navigationOrchestrator.getSecondaryNavigation(), { isActive: true });
+        if (!rootNavigationItem) {
+          throw new FifthweekError('Could not determine active secondary page for breadcrumb');
+        }
+
+        scope.rootName = rootNavigationItem.name;
+        scope.rootIcon = rootNavigationItem.icon;
+        scope.rootState = rootNavigationItem.state;
+      }
     }
   };
 });
