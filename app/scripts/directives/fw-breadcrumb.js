2e72b4105bfbeeb6df58065217ee4a20d1f6c6ae
Lawrence Wagerfield <lawrence@wagerfield.com>
2015-02-22 13:41:39 +0000

    Add psuedo child states for manage collection breadcrumb

--- app/scripts/directives/fw-breadcrumb.js
+++ app/scripts/directives/fw-breadcrumb.js
@@ -4,12 +4,15 @@ angular.module('webApp').directive('fwBreadcrumb', function (navigationOrchestra
   return {
     restrict: 'E',
     scope: {
-      intermediateStates: '=',
-      current: '='
+      intermediateStates: '&',
+      current: '@',
+      // Convenience bindings for creating intermediate states:
+      previous: '@',
+      previousState: '&'
     },
     templateUrl: 'views/partials/breadcrumb.html',
     link: {
-      pre: function (scope) {
+      pre: function (scope, element, attrs) {
         var rootNavigationItem = _.find(navigationOrchestrator.getSecondaryNavigation(), { isActive: true });
         if (!rootNavigationItem) {
           throw new FifthweekError('Could not determine active secondary page for breadcrumb');
@@ -18,6 +21,19 @@ angular.module('webApp').directive('fwBreadcrumb', function (navigationOrchestra
         scope.rootName = rootNavigationItem.name;
         scope.rootIcon = rootNavigationItem.icon;
         scope.rootState = rootNavigationItem.state;
+
+        if (attrs.previous && attrs.previousState) {
+          scope.intermediateStates = [
+            {
+              name: scope.previous,
+              click: scope.previousState
+            }
+          ];
+        }
+        else {
+          // Evaluate states once.
+          scope.intermediateStates = scope.intermediateStates();
+        }
       }
     }
   };
