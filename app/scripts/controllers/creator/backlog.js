832b4066263de1dd66c5128ed646b68dcfd4f9fc
Lawrence Wagerfield <lawrence@wagerfield.com>
2015-02-22 19:09:50 +0000

    Restyle schedule mode info on backlog view

--- app/scripts/controllers/creator/backlog.js
+++ app/scripts/controllers/creator/backlog.js
@@ -12,18 +12,21 @@ angular.module('webApp').controller('backlogCtrl',
         //     imageId: 'Base64Guid', /* optional */
         //     scheduledByQueue: false,
         //     liveDate: '2015-12-25T14:45:05Z'
+        liveIn:'1 day',
         dayOfWeek:'Wed',
         date:'21st Jan',
         bodyText:'Hang in there folks, nearly ready!',
         queued:false
       },
       {
+        liveIn:'3 days',
         dayOfWeek:'Fri',
         date:'23rd Jan',
         bodyText:'Looking good :-)',
         queued:true
       },
       {
+        liveIn:'5 days',
         dayOfWeek:'Sun',
         date:'25th Jan',
         bodyText:'Almost there',
