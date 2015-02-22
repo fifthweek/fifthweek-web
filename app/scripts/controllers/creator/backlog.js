842273c4aa8eb886d5a679c733f7573d30fed2dc
Lawrence Wagerfield <lawrence@wagerfield.com>
2015-02-22 22:28:38 +0000

    Add channel and collection indicators to backlog posts

--- app/scripts/controllers/creator/backlog.js
+++ app/scripts/controllers/creator/backlog.js
@@ -16,13 +16,15 @@ angular.module('webApp').controller('backlogCtrl',
         dayOfWeek:'Wed',
         date:'21st Jan',
         bodyText:'Hang in there folks, nearly ready!',
-        queued:false
+        collection:'Side Comic',
+        queued:true
       },
       {
         liveIn:'3 days',
         dayOfWeek:'Fri',
         date:'23rd Jan',
         bodyText:'Looking good :-)',
+        collection:'Blog',
         queued:true
       },
       {
@@ -30,9 +32,17 @@ angular.module('webApp').controller('backlogCtrl',
         dayOfWeek:'Sun',
         date:'25th Jan',
         bodyText:'Almost there',
+        channel:'Everyone',
         queued:false
+      },
+      {
+        liveIn:'6 days',
+        dayOfWeek:'Mon',
+        date:'26th Jan',
+        bodyText:'Very nearly there',
+        channel:'HD Channel',
+        queued:true
       }
     ];
-
   }
 );
