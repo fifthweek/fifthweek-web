3a9691d4fc32e9236a3172b70b88999f53ed6c79
ttbarnes <tony@tonybarnes.me>
2015-02-23 21:39:29 +0000

    backlog unit test, test for modal function

---
+++ test/spec/controllers/creator/backlog.js
@@ -0,0 +1,53 @@
+'use strict';
+
+describe('backlog controller', function() {
+  var nextState = 'nextState';
+  var $q;
+  var scope;
+  var $state;
+  var calculatedStates;
+  var $modal;
+  var authenticationService;
+  var target;
+
+  beforeEach(function() {
+    calculatedStates = jasmine.createSpyObj('calculatedStates', [ 'getDefaultState' ]);
+    $modal = {};
+    authenticationService = jasmine.createSpyObj('utilities', ['signIn', 'registerUser']);
+
+    module('webApp', 'stateMock');
+    module(function($provide) {
+      $provide.value('calculatedStates', calculatedStates);
+      $provide.value('$modal', $modal);
+      $provide.value('authenticationService', authenticationService);
+    });
+
+    inject(function($injector) {
+      $q = $injector.get('$q');
+      var $controller = $injector.get('$controller');
+      scope = $injector.get('$rootScope').$new();
+      scope.form = {};
+      $state = $injector.get('$state');
+      target = $controller('backlogCtrl', { $scope: scope });
+    });
+
+    // Satisfy the 'construction precondition' by default. There only needs to be
+    // one test to ensure appropriate behaviour when this condition is not met.
+    authenticationService.currentUser = { authenticated: false };
+
+    initializeTarget();
+  });
+
+  afterEach(function(){
+    $state.verifyNoOutstandingTransitions();
+  });
+
+  // Split out since we have logic running in the constructor.
+  function initializeTarget() {
+  }
+
+  it('should contain an openModalImage function', function(){
+    expect(scope.openModalImage).toBeDefined();
+  });
+
+});
