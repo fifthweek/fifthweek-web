6dc897658b77ec3dc8d2d416df73ec1ae181c1a4
Lawrence Wagerfield <lawrence@wagerfield.com>
2015-02-23 19:13:59 +0000

    Fix broken ID in test

--- test/e2e/pages/sidebar.page.js
+++ test/e2e/pages/sidebar.page.js
@@ -6,7 +6,7 @@ SidebarPage.prototype = Object.create({}, {
   links: { get: function () { return element.all(by.css('#sidebar ul a')); }},
   signInLink: { get: function () { return element(by.id('navigation-sign-in')); }},
   registerLink: { get: function () { return element(by.id('navigation-register')); }},
-  createSubscriptionLink: { get: function () { return element(by.id('navigation-create-your-subscription')); }},
+  createSubscriptionLink: { get: function () { return element(by.id('navigation-create-subscription')); }},
   settingsLink: { get: function () { return element(by.id('navigation-settings')); }},
   helpLink: { get: function () { return element(by.id('navigation-help')); }}
 });
