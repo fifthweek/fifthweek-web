a2123a5e1c898f044c88254a995b70a0531c1fec
Lawrence Wagerfield <lawrence@wagerfield.com>
2015-02-24 11:51:37 +0000

    Update new image form to use collection name directive

--- app/scripts/directives/fw-form-input-channel-price.js
+++ app/scripts/directives/fw-form-input-channel-price.js
@@ -6,7 +6,7 @@ angular.module('webApp').directive('fwFormInputChannelPrice', function (utilitie
     scope: true,
     templateUrl: 'views/partials/form-input-channel-price.html',
     link: function(scope, element, attrs) {
-      scope.hideHelp = utilities.parseFlag(attrs, 'hideHelp');
+      scope.showHelp = utilities.parseFlag(attrs, 'showHelp');
       utilities.forDirective(scope, element, attrs).scaffoldFormInput();
     }
   };
--- app/scripts/directives/fw-form-input-collection-name.js
+++ app/scripts/directives/fw-form-input-collection-name.js
@@ -6,6 +6,7 @@ angular.module('webApp').directive('fwFormInputCollectionName', function (utilit
     scope: true,
     templateUrl: 'views/partials/form-input-collection-name.html',
     link: function(scope, element, attrs) {
+      scope.showHelp = utilities.parseFlag(attrs, 'showHelp');
       utilities.forDirective(scope, element, attrs).scaffoldFormInput();
     }
   };
