73fafb037be6fc936d5a6fefbfa29c611c73689a
Lawrence Wagerfield <lawrence@wagerfield.com>
2015-02-24 10:46:40 +0000

    Style initial state of edit file, image and note backlog posts

--- app/scripts/controllers/creator/backlog-edit-post.js
+++ app/scripts/controllers/creator/backlog-edit-post.js
@@ -1,6 +1,21 @@
 angular.module('webApp').controller('backlogEditPostCtrl',
-  function() {
+  function($scope, postId) {
     'use strict';
 
+    $scope.model = {
+      isNote: false,
+      isImage: false,
+      isFile: false
+    };
+
+    if (postId === 'a') {
+      $scope.model.isNote = true;
+    }
+    else if (postId === 'b') {
+      $scope.model.isImage = true;
+    }
+    else {
+      $scope.model.isFile = true;
+    }
   }
 );
