4e8f96c2c22939fb0dc002959403118403bc02a8
Lawrence Wagerfield <lawrence@wagerfield.com>
2015-02-26 15:50:56 +0000

    Move MD directive into source code

---
+++ app/scripts/directives/angular-md.js
@@ -0,0 +1,18 @@
+'use strict';
+angular.module('yaru22.md', []).directive('md', function () {
+  return {
+    restrict: 'E',
+    require: '?ngModel',
+    link: function ($scope, $elem, $attrs, ngModel) {
+      if (!ngModel) {
+        var html = marked($elem.text());
+        $elem.html(html);
+        return;
+      }
+      ngModel.$render = function () {
+        var html = marked(ngModel.$viewValue || '');
+        $elem.html(html);
+      };
+    }
+  };
+});
