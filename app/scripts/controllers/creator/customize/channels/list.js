0e5aa26952c07787f91f60a18ea34e03102b050c
Lawrence Wagerfield <lawrence@wagerfield.com>
2015-03-03 14:57:12 +0000

    Move error icon into CSS. Initial channel list integration

--- app/scripts/controllers/creator/customize/channels/list.js
+++ app/scripts/controllers/creator/customize/channels/list.js
@@ -1,11 +1,39 @@
-angular.module('webApp').controller('listChannelsCtrl', function($scope, $state, states) {
+angular.module('webApp').controller('listChannelsCtrl', function($scope, $q, aggregateUserState, errorFacade) {
   'use strict';
 
-  $scope.model = {
-    channels: [
-      {id:'a', name:'Base Channel', price:'0.99', description:['Backstage News Feed', 'Eternal Gratitude']},
-      {id:'b', name:'Extras Channel', price:'0.75', description:['Weekly exclusive wallpapers', 'New side comic']},
-      {id:'c', name:'HD Channel', price:'0.25', description:['Signed digital prints', 'Behind the scenes production videos', 'Behind the scenes photos']}
-    ]
-  }
+  $scope.model = {};
+
+  // Extract into utilities
+  var getChannelsAndCollections = function() {
+    if(!aggregateUserState.currentValue){
+      return $q.reject(new FifthweekError('No aggregate state found.'));
+    }
+
+    var channels = aggregateUserState.currentValue.createdChannelsAndCollections.channels;
+
+    if(channels.length === 0) {
+      return $q.reject(new DisplayableError('You must create a subscription before posting.'));
+    }
+
+    return $q.when(channels);
+  };
+
+  getChannelsAndCollections()
+    .then(function(channels) {
+      $scope.model = {
+        channels: _.map(channels, function(channel) {
+          return {
+            id: channel.channelId,
+            name: channel.name || 'Base Channel',
+            price: (channel.priceInUsCentsPerWeek / 100).toFixed(2),
+            description: channel.description.split('\n')
+          };
+        })
+      };
+    })
+    .catch(function(error) {
+      return errorFacade.handleError(error, function(message) {
+        $scope.model.errorMessage = message;
+      });
+    });
 });
