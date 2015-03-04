d6bcdf03aa7649777533bf1f03835ff33b25f949
Lawrence Wagerfield <lawrence@wagerfield.com>
2015-03-04 11:28:19 +0000

    Test base channel appears after signing back in

--- test/e2e/spec/creators/customize/channel-list-form.spec.js
+++ test/e2e/spec/creators/customize/channel-list-form.spec.js
@@ -1,5 +1,7 @@
+var HomePage = require('../../../pages/home.page.js');
 var RegisterPage = require('../../../pages/register.page.js');
 var SignOutPage = require('../../../pages/sign-out.page.js');
+var SignInPage = require('../../../pages/sign-in.page.js');
 var SidebarPage = require('../../../pages/sidebar.page.js');
 var HeaderCustomizePage = require('../../../pages/header-customize.page.js');
 var CreateSubscriptionPage = require('../../../pages/creators/create-subscription.page.js');
@@ -9,9 +11,12 @@ var ChannelListPage = require('../../../pages/creators/customize/channel-list.pa
 describe('channel list form', function() {
   'use strict';
 
+  var registration;
   var subscription;
 
+  var homePage = new HomePage();
   var signOutPage = new SignOutPage();
+  var signInPage = new SignInPage();
   var registerPage = new RegisterPage();
   var createSubscriptionPage = new CreateSubscriptionPage();
   var channelAddPage = new ChannelAddPage();
@@ -19,26 +24,38 @@ describe('channel list form', function() {
   var header = new HeaderCustomizePage();
   var page = new ChannelListPage();
 
+  it('should run once before all', function() {
+    signOutPage.signOutAndGoHome();
+    registration = registerPage.registerSuccessfully();
+    subscription = createSubscriptionPage.submitSuccessfully();
+  });
+
+  it('should allow new channels to be created', function () {
+    navigateToPage();
+    page.addChannelButton.click();
+
+    expect(browser.getCurrentUrl()).toContain(channelAddPage.pageUrl);
+  });
+
+  it('should contain the base channel after registering', function () {
+    navigateToPage();
+    expectBaseChannel();
+  });
+
+  it('should contain the base channel after registering, after signing back in', function () {
+    signOutPage.signOutAndGoHome();
+    homePage.signInLink.click();
+    signInPage.signInSuccessfully(registration.username, registration.password);
+    navigateToPage();
+    expectBaseChannel();
+  });
+
   var navigateToPage = function() {
     sidebar.customizeLink.click();
     header.channelsLink.click();
   };
 
-  it('should run once before all', function() {
-    signOutPage.signOutAndGoHome();
-    registerPage.registerSuccessfully();
-    subscription = createSubscriptionPage.submitSuccessfully();
-    navigateToPage();
-  });
-
-  afterEach(navigateToPage);
-
-  it('should allow new channels to be created', function () {
-    page.addChannelButton.click();
-    expect(browser.getCurrentUrl()).toContain(channelAddPage.pageUrl);
-  });
-
-  it('should contain the base channel after registering', function () {
+  var expectBaseChannel = function() {
     expect(page.channels.count()).toBe(1);
 
     var defaultChannelText = page.getChannel(0).getText();
@@ -46,5 +63,5 @@ describe('channel list form', function() {
     expect(defaultChannelText).toContain('$' + subscription.basePrice);
     expect(defaultChannelText).toContain('Exclusive News Feed');
     expect(defaultChannelText).toContain('Early Updates on New Releases');
-  });
+  };
 });
