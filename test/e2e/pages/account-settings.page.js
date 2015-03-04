0c7168b6ccba7d01d0f7d7022269edb7fb7e52d4
James Thurley <mail@jamesthurley.com>
2015-03-04 15:49:40 +0000

    Complete account settings protractor tests

--- test/e2e/pages/account-settings.page.js
+++ test/e2e/pages/account-settings.page.js
@@ -3,8 +3,9 @@
 var AccountPage = function() {};
 
 AccountPage.prototype = Object.create({}, {
-  fileInput: { get: function() { return element(by.css('.file-upload-button-area input')); }},
+  fileInput: { get: function() { return element(by.css('#file-upload-button-area input')); }},
   profileImage: { get: function(){ return element(by.css('.available-image')); }},
+  noProfileImage: { get: function(){ return element(by.css('.blank-area')); }},
   emailTextBox: { get: function(){ return element(by.model('model.accountSettings.email')); }},
   usernameTextBox: { get: function(){ return element(by.model('model.accountSettings.username')); }},
   passwordTextBox: { get: function(){ return element(by.id('model-password')); }},
@@ -12,7 +13,8 @@ AccountPage.prototype = Object.create({}, {
   savedSuccessfullyMessage: { get: function(){ return element(by.css('.alert-success')); }},
   helpMessages: { get: function () { return element.all(by.css('#accountSettingsForm .help-block')); }},
   setFileInput: { value: function(filePath) {
-    var absolutePath = path.resolve(__dirname, filePath);
+    var absolutePath = __dirname + '/' + filePath;
+    console.log(absolutePath);
     this.fileInput.sendKeys(absolutePath);
   }}
 });
--- test/e2e/spec/account-settings-form.spec.js
+++ test/e2e/spec/account-settings-form.spec.js
@@ -31,19 +31,29 @@ describe('account settings form', function() {
       navigateToPage();
     });
 
+    it('should not be able to save without changes', function(){
+      expect(page.saveChangesButton.isEnabled()).toBe(false);
+    });
+
     describe('when saving new data', function(){
 
       afterEach(function() {
         page.saveChangesButton.click();
-        expect(page.savedSuccessfullyMessage.isDisplayed()).toBe(true);
 
+        // Test page behaviour
+        expect(page.savedSuccessfullyMessage.isDisplayed()).toBe(true);
+        expect(page.saveChangesButton.isEnabled()).toBe(false);
+
+        // Reset state
         page.emailTextBox.clear();
         page.emailTextBox.sendKeys(registration.email);
         page.usernameTextBox.clear();
         page.usernameTextBox.sendKeys(registration.username);
         page.passwordTextBox.clear();
 
+        // Test page behaviour
         expect(page.savedSuccessfullyMessage.isDisplayed()).toBe(false);
+        expect(page.saveChangesButton.isEnabled()).toBe(true);
       });
 
       it('should allow the user to change their email', function(){
@@ -84,5 +94,46 @@ describe('account settings form', function() {
     usernameInputPage.includeSadPaths(page.usernameTextBox, page.saveChangesButton, page.helpMessages, function() {});
 
     passwordInputPage.includeSadPaths(page.passwordTextBox, page.saveChangesButton, page.helpMessages, function() {}, true);
+  });
+
+  describe('when setting form to known data', function(){
+
+    it('should run once before all', function() {
+      navigateToPage();
+    });
+
+    it('should display a thumbnail', function(){
+      expect(page.noProfileImage.isDisplayed()).toBe(true);
+      page.setFileInput('../sample-image.jpg');
+      browser.wait(page.profileImage.isDisplayed);
+      expect(page.profileImage.isDisplayed()).toBe(true);
+    });
+
+    it('should persist changes', function(){
+      registration.email = 'phil+' + registration.email;
+      page.emailTextBox.clear();
+      page.emailTextBox.sendKeys(registration.email);
+
+      registration.username = 'ph_' + registration.username.substring(3);
+      page.usernameTextBox.clear();
+      page.usernameTextBox.sendKeys(registration.username);
+
+      registration.password = 'phil-the-cat';
+      page.passwordTextBox.clear();
+      page.passwordTextBox.sendKeys(registration.password);
+
+      page.saveChangesButton.click();
+      expect(page.savedSuccessfullyMessage.isDisplayed()).toBe(true);
+      expect(page.saveChangesButton.isEnabled()).toBe(false);
+
+      commonWorkflows.reSignIn(registration);
+      sidebar.settingsLink.click();
+
+      browser.wait(page.profileImage.isDisplayed);
+      expect(page.profileImage.isDisplayed()).toBe(true);
+
+      expect(page.emailTextBox.getAttribute('value')).toBe(registration.email);
+      expect(page.usernameTextBox.getAttribute('value')).toBe(registration.username);
+    });
   });
 });
