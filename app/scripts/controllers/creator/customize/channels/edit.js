bf3523ebd01c9af1f894025ac2c5862dc2b81494
Lawrence Wagerfield <lawrence@wagerfield.com>
2015-03-04 12:51:17 +0000

    Initial integration of edit channelview

--- app/scripts/controllers/creator/customize/channels/edit.js
+++ app/scripts/controllers/creator/customize/channels/edit.js
@@ -1,5 +1,25 @@
-angular.module('webApp').controller('editChannelCtrl', function($scope, $state, states) {
+angular.module('webApp').controller('editChannelCtrl', function($scope, $state, aggregateUserStateUtilities, errorFacade) {
   'use strict';
+
+  var channelId = $state.params.id;
+  $scope.model = {};
+
+  aggregateUserStateUtilities.getChannelsAndCollections()
+    .then(function(channels) {
+      $scope.model.savedChannel = _.find(channels, { channelId: channelId });
+      if (!$scope.model.savedChannel) {
+        throw new FifthweekError('Channel does not exist');
+      }
+
+      $scope.model.channel = _.cloneDeep($scope.model.savedChannel);
+      $scope.model.channel.price = ($scope.model.channel.priceInUsCentsPerWeek / 100).toFixed(2);
+      delete $scope.model.channel.priceInUsCentsPerWeek;
+    })
+    .catch(function(error) {
+      return errorFacade.handleError(error, function(message) {
+        $scope.model.errorMessage = message;
+      });
+    });
 
   $scope.delete = function() {
     $state.go(states.creators.customize.channels.name);
--- app/scripts/controllers/creator/customize/channels/list.js
+++ app/scripts/controllers/creator/customize/channels/list.js
@@ -1,20 +1,21 @@
-angular.module('webApp').controller('listChannelsCtrl', function($scope, $q, aggregateUserStateUtilities, errorFacade) {
+angular.module('webApp').controller('listChannelsCtrl', function($scope, aggregateUserStateUtilities, errorFacade) {
   'use strict';
 
   $scope.model = {};
 
   aggregateUserStateUtilities.getChannelsAndCollections()
     .then(function(channels) {
-      $scope.model = {
-        channels: _.map(channels, function(channel) {
+      $scope.model.channels = _.map(
+        channels,
+        function(channel) {
           return {
             id: channel.channelId,
             name: channel.name,
             price: (channel.priceInUsCentsPerWeek / 100).toFixed(2),
             description: channel.description.split('\n')
           };
-        })
-      };
+        }
+      );
     })
     .catch(function(error) {
       return errorFacade.handleError(error, function(message) {
