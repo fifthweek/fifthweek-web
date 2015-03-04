3da2583805caad1273d413e8c346715fe4941f9d
Lawrence Wagerfield <lawrence@wagerfield.com>
2015-03-04 17:18:25 +0000

    Initial e2e tests for edit channel form

--- test/e2e/pages/creators/customize/channel-edit.page.js
+++ test/e2e/pages/creators/customize/channel-edit.page.js
@@ -3,6 +3,17 @@
 var ChannelEditPage = function() {};
 
 ChannelEditPage.prototype = Object.create({}, {
+  nameTextBox: { get: function () { return element(by.id('model-channel-name')); }},
+  descriptionTextBox: { get: function () { return element(by.id('model-channel-description')); }},
+  priceTextBox: { get: function () { return element(by.id('model-channel-price')); }},
+  hiddenCheckboxSelector: { get: function () { return by.id('model-channel-hidden'); }},
+  hiddenCheckbox: { get: function () { return element(this.hiddenCheckboxSelector); }},
+  hiddenCheckboxCount: { get: function () { return element.all(this.hiddenCheckboxSelector).count(); }},
+  saveButton: { get: function () { return element(by.id('save-channel-button')); }},
+  cancelButton: { get: function () { return element(by.id('cancel-button')); }},
+  deleteButtonSelector: { get: function () { return by.id('delete-channel-link'); }},
+  deleteButton: { get: function () { return element(this.deleteButtonSelector); }},
+  deleteButtonCount: { get: function () { return element.all(this.deleteButtonSelector).count(); }}
 });
 
 module.exports = ChannelEditPage;
---
+++ test/e2e/spec/creators/customize/channel-edit-form.spec.js
@@ -0,0 +1,59 @@
+var CommonWorkflows = require('../../../common-workflows.js');
+var SidebarPage = require('../../../pages/sidebar.page.js');
+var HeaderCustomizePage = require('../../../pages/header-customize.page.js');
+var ChannelListPage = require('../../../pages/creators/customize/channel-list.page.js');
+var ChannelEditPage = require('../../../pages/creators/customize/channel-edit.page.js');
+
+describe('edit channel form', function() {
+  'use strict';
+
+  var registration;
+  var subscription;
+
+  var commonWorkflows = new CommonWorkflows();
+  var sidebar = new SidebarPage();
+  var header = new HeaderCustomizePage();
+  var channelListPage = new ChannelListPage();
+  var page = new ChannelEditPage();
+
+  it('should run once before all', function() {
+    var context = commonWorkflows.createSubscription();
+    registration = context.registration;
+    subscription = context.subscription;
+    sidebar.customizeLink.click();
+    header.channelsLink.click();
+  });
+
+  describe('when editing the default channel', function() {
+    it('should run once before all', function() {
+      channelListPage.getEditChannelButton(0).click();
+    });
+
+    it('should initialise with the correct properties', function() {
+      expect(page.nameTextBox.getAttribute('value')).toBe(channelListPage.defaultChannelName);
+      expect(page.descriptionTextBox.getAttribute('value')).toBe(channelListPage.defaultChannelDescription);
+      expect(page.priceTextBox.getAttribute('value')).toBe(subscription.basePrice);
+    });
+
+    it('should not give you the option to delete the channel', function() {
+      expect(page.deleteButtonCount).toBe(0);
+    });
+
+    it('should not give you the option to hide the channel', function() {
+      expect(page.hiddenCheckboxCount).toBe(0);
+    });
+  });
+
+  //describe('when editing a non-default channel', function() {
+  //  it('should initialise with the correct properties', function() {
+  //    expect(page.nameTextBox.getAttribute('value')).toBe(...);
+  //    expect(page.descriptionTextBox.getAttribute('value')).toBe(...);
+  //    expect(page.priceTextBox.getAttribute('value')).toBe(...);
+  //    expect(page.hiddenCheckbox.getAttribute('value')).toBe(...);
+  //  });
+  //
+  //  describe('when deleting the channel', function() {
+  //
+  //  });
+  //});
+});
