948437ac97026d39654e7540f57e25bfe75e765a
Lawrence Wagerfield <lawrence@wagerfield.com>
2015-03-06 13:14:52 +0000

    e2e test add channel page

---
+++ test/e2e/spec/creators/customize/channel-add-page.spec.js
@@ -0,0 +1,31 @@
+var CommonWorkflows = require('../../../common-workflows.js');
+var SidebarPage = require('../../../pages/sidebar.page.js');
+var HeaderCustomizePage = require('../../../pages/header-customize.page.js');
+var BreadcrumbPage = require('../../../pages/breadcrumb.page.js');
+var ChannelListPage = require('../../../pages/creators/customize/channel-list.page.js');
+
+ddescribe('add channel page', function() {
+  'use strict';
+
+  var commonWorkflows = new CommonWorkflows();
+  var sidebar = new SidebarPage();
+  var header = new HeaderCustomizePage();
+  var breadcrumb = new BreadcrumbPage();
+  var channelListPage = new ChannelListPage();
+
+  it('should run once before all', function() {
+    commonWorkflows.createSubscription();
+    sidebar.customizeLink.click();
+    header.channelsLink.click();
+    channelListPage.addChannelButton.click();
+  });
+
+  breadcrumb.includeTests(['Channels', 'New Channel'], function() {
+    header.channelsLink.click();
+    channelListPage.addChannelButton.click();
+  });
+
+  header.includeBasicTests(header.channelsLink);
+
+  sidebar.includeEstablishedCreatorTests(sidebar.customizeLink);
+});
