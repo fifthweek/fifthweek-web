60baa391c6d1f168f9016b0850d2a77b6aebd55b
James Thurley <mail@jamesthurley.com>
2015-03-06 13:12:00 +0000

    Initial compose note page file

--- test/e2e/pages/creators/compose/compose-note.page.js
+++ test/e2e/pages/creators/compose/compose-note.page.js
@@ -2,8 +2,48 @@
 
 var composeNotePage = function() {};
 
+
 composeNotePage.prototype = Object.create({}, {
-  pageUrl: { get: function () { return '/creators/compose/note'; }}
+
+  postNowButton: { get: function() { return element(by.css('.btn[fw-form-submit="postNow()"]')); }},
+  postLaterButton: { get: function() { return element(by.css('.btn[fw-form-submit="postLater()"]')); }},
+
+  postToBacklogButton: { get: function() { return element(by.css('.btn[fw-form-submit="postToBacklog()"]')); }},
+  cancelButton: { get: function() { return element(by.css('.btn[ng-click="cancelPostLater()"]')); }},
+
+  contentTextBox: { get: function() { return element(by.id('model-input-note')); }},
+  channelSelect: { get: function() { return element(by.id('model-input-selected-channel')); }},
+  datepicker: { get: function() { return element(by.id('model-input-date')); }},
+
+  pageUrl: { get: function () { return '/creators/compose/note'; }},
+
+  populateContent: { value: function(channelName){
+    var date = new Date();
+    this.contentTextBox.clear();
+    this.contentTextBox.sendKeys('Note on ' + date.toISOString());
+
+    if(channelName){
+      element(by.cssContainingText('#model-input-selected-channel option', channelName)).click();
+    }
+  }},
+
+  postNoteNow: { value: function(channelName) {
+    this.populateContent(channelName);
+
+    this.postNowButton.click();
+  }},
+
+  postNoteOnDate: { value: function(channelName) {
+    this.populateContent(channelName);
+
+    this.postLaterButton.click();
+
+    var tomorrow = new Date(new Date().getTime() + 24*60*60*1000);
+    this.datepicker.clear();
+    this.datepicker.sendKeys(tomorrow.toISOString());
+
+    this.postToBacklogButton.click();
+  }}
 });
 
 module.exports = composeNotePage;
