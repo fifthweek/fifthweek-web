d224abb514305e2eca78fe13d8eb8977337671a8
Lawrence Wagerfield <lawrence@wagerfield.com>
2015-03-07 13:22:37 +0000

    Unit test release time formatting

--- app/scripts/services/release-time-formatter.js
+++ app/scripts/services/release-time-formatter.js
@@ -2,19 +2,29 @@ angular.module('webApp').factory('releaseTimeFormatter', function() {
   'use strict';
 
   var service = {};
+  var daysOfWeek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+  ];
+
+  service.getDayOfWeek = function(hourOfWeek) {
+    if (!_.isNumber(hourOfWeek)) {
+      throw new FifthweekError('Expected an hour of week');
+    }
+    if (hourOfWeek < 0 || hourOfWeek > 167) {
+      throw new FifthweekError('Hour of week must be between 0 and 167');
+    }
+
+    return daysOfWeek[Math.floor(hourOfWeek / 24)];
+  };
 
   service.getDaysOfWeek = function(hoursOfWeek) {
-    return _.map(hoursOfWeek, function(hourOfWeek) {
-      return [
-        'Sunday',
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday'
-      ][Math.floor(hourOfWeek / 24)];
-    })
+    return _.map(hoursOfWeek, service.getDayOfWeek);
   };
 
   return service;
---
+++ test/spec/services/release-time-formatter.js
@@ -0,0 +1,94 @@
+describe('release time formatter', function() {
+  'use strict';
+
+  var target;
+
+  beforeEach(function() {
+    module('webApp');
+    inject(function($injector) {
+      target = $injector.get('releaseTimeFormatter');
+    });
+  });
+
+  describe('when getting the day of week', function() {
+    it('requires a numerical input', function() {
+      expect(function() {
+        target.getDayOfWeek();
+      }).toThrowError(FifthweekError);
+
+      expect(function() {
+        target.getDayOfWeek('hello');
+      }).toThrowError(FifthweekError);
+
+      expect(function() {
+        target.getDayOfWeek({});
+      }).toThrowError(FifthweekError);
+    });
+
+    it('requires non-negative numbers', function() {
+      expect(function() {
+        target.getDayOfWeek(-1);
+      }).toThrowError(FifthweekError);
+    });
+
+    it('requires numbers under 167 are provided', function() {
+      expect(function() {
+        target.getDayOfWeek(168);
+      }).toThrowError(FifthweekError);
+    });
+
+    it('should associate a different day with each 24 hour block', function() {
+      // Day 1
+      expect(target.getDayOfWeek(0)).toBe(target.getDayOfWeek(23));
+      expect(target.getDayOfWeek(23)).not.toBe(target.getDayOfWeek(24));
+
+      // Day 2
+      expect(target.getDayOfWeek(24)).toBe(target.getDayOfWeek(47));
+      expect(target.getDayOfWeek(47)).not.toBe(target.getDayOfWeek(48));
+
+      // Day 3
+      expect(target.getDayOfWeek(48)).toBe(target.getDayOfWeek(71));
+      expect(target.getDayOfWeek(71)).not.toBe(target.getDayOfWeek(72));
+
+      // Day 4
+      expect(target.getDayOfWeek(72)).toBe(target.getDayOfWeek(95));
+      expect(target.getDayOfWeek(95)).not.toBe(target.getDayOfWeek(96));
+
+      // Day 5
+      expect(target.getDayOfWeek(96)).toBe(target.getDayOfWeek(119));
+      expect(target.getDayOfWeek(119)).not.toBe(target.getDayOfWeek(120));
+
+      // Day 6
+      expect(target.getDayOfWeek(120)).toBe(target.getDayOfWeek(143));
+      expect(target.getDayOfWeek(143)).not.toBe(target.getDayOfWeek(144));
+
+      // Day 7
+      expect(target.getDayOfWeek(144)).toBe(target.getDayOfWeek(167));
+    });
+
+    it('should start from Sunday', function() {
+      expect(target.getDayOfWeek(0)).toBe('Sunday');
+    });
+
+    it('should cover each of the 7 days', function() {
+      expect(target.getDayOfWeek(0)).toBe('Sunday');
+      expect(target.getDayOfWeek(24)).toBe('Monday');
+      expect(target.getDayOfWeek(48)).toBe('Tuesday');
+      expect(target.getDayOfWeek(72)).toBe('Wednesday');
+      expect(target.getDayOfWeek(96)).toBe('Thursday');
+      expect(target.getDayOfWeek(120)).toBe('Friday');
+      expect(target.getDayOfWeek(144)).toBe('Saturday');
+    });
+  });
+
+  describe('when getting the days of week', function() {
+    it('should map inputs to the "getDayOfWeek" function', function() {
+      var range = 0;
+      target.getDayOfWeek = function() {
+        return ++range;
+      };
+
+      expect(target.getDaysOfWeek(['a', 'b', 'c'])).toEqual([1, 2, 3]);
+    });
+  });
+});
