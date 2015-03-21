6f783f4d7c1ff2dc19767c9376d2bae838a2796d
James Thurley <mail@jamesthurley.com>
2015-03-21 09:59:56 +0000

    Revert "Revert "Add initial protractor tests for creator-timeline""
    This reverts commit 372a7394f83e72c466531dae169608109f3f23d4.

--- test/e2e/pages/post.page.js
+++ test/e2e/pages/post.page.js
@@ -13,7 +13,7 @@
 
   PostPage.prototype = Object.create({}, {
     postIndex: { value: 0, writable: true },
-    postId: { get: function() { return element(by.id(createPostSelector(this.postIndex)))}},
+    postId: { get: function() { return element(by.id(createPostSelector(this.postIndex))); }},
     byCss: { value: function(css){
       return by.css(createPostSelector(this.postIndex) + ' ' + css);
     }},
@@ -21,21 +21,21 @@
       return by.cssContainingText(createPostSelector(this.postIndex) + ' ' + css, text);
     }},
 
-    postsArea: { get: function() { return element(by.css('.posts'))}},
-    taggedPostsArea: { get: function() { return element(by.css('.tagged-posts'))}},
+    postsArea: { get: function() { return element(by.css('.posts')); }},
+    taggedPostsArea: { get: function() { return element(by.css('.tagged-posts')); }},
     allPosts: { get: function () { return element.all(by.css('.posts .post')); }},
 
-    image: { get: function() { return element(this.byCss('.full-width-image'))}},
-    scheduleTag: { get: function() { return element(this.byCss('.tag'))}},
-    comment: { get: function() { return element(this.byCss('.text .content'))}},
-    fileDownloadLink: { get: function() { return element(this.byCss('.text .content .file-download'))}},
-    profileImage: { get: function() { return element(this.byCss('.author-image'))}},
-    usernameLink: { get: function() { return element(this.byCss('.poster-name'))}},
-    containerNameLink: { get: function() { return element(this.byCss('.container-name'))}},
-    liveInLink: { get: function() { return element(this.byCss('.live-in-info'))}},
-    moreActionsButton: { get: function() { return element(this.byCss('.actions-more button'))}},
-    editPostLink: { get: function() { return element(this.byCssContainingText('.actions-drop-down a', 'Edit'))}},
-    deletePostLink: { get: function() { return element(this.byCssContainingText('.actions-drop-down a', 'Delete'))}}
+    image: { get: function() { return element(this.byCss('.full-width-image')); }},
+    scheduleTag: { get: function() { return element(this.byCss('.tag')); }},
+    comment: { get: function() { return element(this.byCss('.text .content')); }},
+    fileDownloadLink: { get: function() { return element(this.byCss('.text .content .file-download')); }},
+    profileImage: { get: function() { return element(this.byCss('.author-image')); }},
+    usernameLink: { get: function() { return element(this.byCss('.poster-name')); }},
+    containerNameLink: { get: function() { return element(this.byCss('.container-name')); }},
+    liveInLink: { get: function() { return element(this.byCss('.live-in-info')); }},
+    moreActionsButton: { get: function() { return element(this.byCss('.actions-more button')); }},
+    editPostLink: { get: function() { return element(this.byCssContainingText('.actions-drop-down a', 'Edit')); }},
+    deletePostLink: { get: function() { return element(this.byCssContainingText('.actions-drop-down a', 'Delete')); }}
   });
 
   module.exports = PostPage;
--- test/e2e/spec/creators/creator-backlog-form.spec.js
+++ test/e2e/spec/creators/creator-backlog-form.spec.js
@@ -2,11 +2,11 @@
   'use strict';
 
   var CommonWorkflows = require('../../common-workflows.js');
-  var SidebarPage = require('../../pages/sidebar.page.js');
-  var HeaderBacklogPage = require('../../pages/header-backlog.page.js');
-  var PostPage = require('../../pages/post.page.js');
+  var SidebarPage = require('../../pages/sidebar.post.js');
+  var HeaderBacklogPage = require('../../pages/header-backlog.post.js');
+  var PostPage = require('../../pages/post.post.js');
 
-  describe('collection list form', function() {
+  describe('creator-backlog form', function() {
 
     var collectionName = 'Cats';
     var filePath = '../../../sample-image-tiny.jpg';
@@ -17,7 +17,7 @@
     var commonWorkflows = new CommonWorkflows();
     var sidebar = new SidebarPage();
     var header = new HeaderBacklogPage();
-    var page = new PostPage();
+    var post = new PostPage();
 
     it('should run once before all', function() {
       var context = commonWorkflows.createSubscription();
@@ -29,70 +29,70 @@
     });
 
     it('should contain no posts after registering', function () {
-      expect(page.allPosts.count()).toBe(0);
+      expect(post.allPosts.count()).toBe(0);
     });
 
     it('should contain no posts after registering, after signing back in', function () {
       commonWorkflows.reSignIn(registration);
       navigateToPage();
-      expect(page.allPosts.count()).toBe(0);
+      expect(post.allPosts.count()).toBe(0);
     });
 
     it('should not show the post after posting a note now', function () {
       commonWorkflows.postNoteNow();
       navigateToPage();
-      expect(page.allPosts.count()).toBe(0);
+      expect(post.allPosts.count()).toBe(0);
     });
 
     it('should show the post after posting a note on a date', function () {
       commonWorkflows.postNoteOnDate();
       navigateToPage();
-      expect(page.allPosts.count()).toBe(1);
-      //expect(page.comment.isDisplayed()).toBe(true);
+      expect(post.allPosts.count()).toBe(1);
+      //expect(post.comment.isDisplayed()).toBe(true);
     });
 
     it('should not show the post after posting a file now', function () {
       commonWorkflows.postFileNow(filePath, collectionName);
       navigateToPage();
-      //expect(page.allPosts.count()).toBe(1);
+      expect(post.allPosts.count()).toBe(1);
     });
 
     it('should show the post after posting a file on a date', function () {
       commonWorkflows.postFileOnDate(filePath, collectionName);
       navigateToPage();
-      page.postIndex = 1;
-      expect(page.allPosts.count()).toBe(2);
-      //expect(page.fileDownloadLink.isDisplayed()).toBe(true);
+      post.postIndex = 1;
+      expect(post.allPosts.count()).toBe(2);
+      //expect(post.fileDownloadLink.isDisplayed()).toBe(true);
     });
 
     it('should show the post after posting a file to the queue', function () {
       commonWorkflows.postFileToQueue(filePath, collectionName);
       navigateToPage();
-      page.postIndex = 2;
-      expect(page.allPosts.count()).toBe(3);
-      //expect(page.fileDownloadLink.isDisplayed()).toBe(true);
+      post.postIndex = 2;
+      expect(post.allPosts.count()).toBe(3);
+      //expect(post.fileDownloadLink.isDisplayed()).toBe(true);
     });
 
     it('should not show the post after posting a image now', function () {
       commonWorkflows.postImageNow(filePath, collectionName);
       navigateToPage();
-      expect(page.allPosts.count()).toBe(3);
+      expect(post.allPosts.count()).toBe(3);
     });
 
     it('should show the post after posting a image on a date', function () {
       commonWorkflows.postImageOnDate(filePath, collectionName);
       navigateToPage();
-      page.postIndex = 3;
-      expect(page.allPosts.count()).toBe(4);
-      //expect(page.image.isDisplayed()).toBe(true);
+      post.postIndex = 3;
+      expect(post.allPosts.count()).toBe(4);
+      //expect(post.image.isDisplayed()).toBe(true);
     });
 
     it('should show the post after posting a image to the queue', function () {
       commonWorkflows.postImageToQueue(filePath, collectionName);
       navigateToPage();
-      page.postIndex = 4;
-      expect(page.allPosts.count()).toBe(5);
-      //expect(page.image.isDisplayed()).toBe(true);
+      post.postIndex = 4;
+      expect(post.allPosts.count()).toBe(5);
+      //expect(post.image.isDisplayed()).toBe(true);
     });
 
     var navigateToPage = function() {
