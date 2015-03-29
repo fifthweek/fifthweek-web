555811ec493ef660ad14e6695a7d0b4571e365e6
Lawrence Wagerfield <lawrence@wagerfield.com>
2015-03-29 00:48:40 +0000

    Remove erronous extra browser from Karma config

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -62,7 +62,7 @@ module.exports = function(config) {
     // - PhantomJS
     // - IE (only Windows)
     browsers: [
-      'Chrome', 'ChromeNoSandbox'
+      'ChromeNoSandbox'
     ],
 
     customLaunchers: {
