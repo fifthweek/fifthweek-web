2393adfb5ef2ae99c8dd9c874b44508cc73cb90b
Lawrence Wagerfield <lawrence@wagerfield.com>
2015-04-03 16:35:20 +0100

    Attempt another fix at collection release time e2e test

--- test/e2e/spec/creators/collection-edit-form.spec.js
+++ test/e2e/spec/creators/collection-edit-form.spec.js
@@ -144,7 +144,7 @@ describe('edit collection form', function() {
       page.newReleaseTimeButton.click();
 
       // Often Protractor complains that it can't find the 'option' elements for this input.
-      testKit.waitForElementToDisplay(element(by.id(page.daySelectId)));
+      browser.wait(element(by.css('#' + page.daySelectId + ' option:first-child')).isPresent);
       releaseTimes.push(testKit.setFormValues(page, page.releaseTimeInputs));
       page.addReleaseTimeButton.click();
 
