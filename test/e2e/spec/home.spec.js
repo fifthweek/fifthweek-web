6275a314b26d7022b10bed0b90572f6ae583695b
Lawrence Wagerfield <lawrence@wagerfield.com>
2015-04-10 20:33:28 +0100

    Revise homepage copy

--- test/e2e/spec/home.spec.js
+++ test/e2e/spec/home.spec.js
@@ -17,14 +17,6 @@ describe("homepage", function() {
     expect(browser.getCurrentUrl()).toContain('/sign-in');
   });
 
-  it('should have correct intro heading', function() {
-    expect(homePage.introHeading.getInnerHtml()).toBe('Comics, photos, <br>videos, blogs.');
-  });
-
-  it('should have correct sub intro heading', function() {
-    expect(homePage.introSubHeading.getText()).toContain('Bring more of your best creations to life. Launch your own subscription');
-  });
-
   it('should link to the "creator journey" video', function() {
     homePage.playVideoLink.click();
     expect(homePage.videoIFrame.getAttribute('src')).toContain('player.vimeo.com/video/114229222');
