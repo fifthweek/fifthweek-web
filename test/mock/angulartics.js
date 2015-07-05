89503853377f178127512116ce549d653d9e4bc9
Lawrence Wagerfield <lawrence@wagerfield.com>
2015-07-05 15:27:37 +0100

    Fix Karma mock for new analytics code

--- test/mock/angulartics.js
+++ test/mock/angulartics.js
@@ -1,6 +1,14 @@
 // Dummy mock for running Karma and still allowing app to find Angulartics modules
 
-angular.module('angulartics', []);
+angular.module('angulartics', []).provider('$analytics', function () {
+
+  // Dummy mock aspects used in app.js configuration.
+  return {
+    $get: function() {},
+    firstPageview: function() {}
+  };
+});
 angular.module('angulartics.kissmetrics', []);
 angular.module('angulartics.google.analytics', []);
 angular.module('angulartics.google.analytics.userid', []);
+angular.module('angulartics.afterFirstPageTrack', []);
