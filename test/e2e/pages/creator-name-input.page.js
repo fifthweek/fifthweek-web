8a3cae9b8cc6824384066eca99ae5685babec8f5
James Thurley <mail@jamesthurley.com>
2015-08-14 11:39:54 +0100

    Add wait in protractor tests

--- test/e2e/pages/creator-name-input.page.js
+++ test/e2e/pages/creator-name-input.page.js
@@ -5,6 +5,10 @@ var testKit = new TestKit();
 
 var CreatorNameInputPage = function() {};
 
+var waitForElementToDisplay = function(inputId){
+  testKit.waitForElementToDisplay(element(by.id(inputId)));
+};
+
 CreatorNameInputPage.prototype = Object.create({},
   {
     newName: { value: function() {
@@ -16,12 +20,14 @@ CreatorNameInputPage.prototype = Object.create({},
       var self = this;
 
       it('should allow numbers, spaces and mixed case in name', function(){
+        waitForElementToDisplay(inputId);
         populateOtherInputsWithValidData();
         testKit.clear(inputId);
         testKit.setValue(inputId, '1 _aA' + self.newName());
       });
 
       it('should allow leading and trailing spaces in name', function(){
+        waitForElementToDisplay(inputId);
         populateOtherInputsWithValidData();
         testKit.clear(inputId);
         testKit.setValue(inputId, ' ' + self.newName() + ' ');
@@ -31,6 +37,7 @@ CreatorNameInputPage.prototype = Object.create({},
       var self = this;
 
       it('requires name', function(){
+        waitForElementToDisplay(inputId);
         populateOtherInputsWithValidData();
         testKit.clear(inputId);
 
@@ -40,6 +47,7 @@ CreatorNameInputPage.prototype = Object.create({},
       });
 
       it('should not allow names with over than 25 characters', function(){
+        waitForElementToDisplay(inputId);
         var maxLength = 25;
         var overSizedValue = self.newName() + new Array(maxLength).join('x');
 
