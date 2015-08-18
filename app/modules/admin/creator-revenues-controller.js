2e7c2c5c4045151b8cb820e908e467406d595cff
James Thurley <mail@jamesthurley.com>
2015-08-18 16:21:44 +0100

    Improve creator revenues form

--- app/modules/admin/creator-revenues-controller.js
+++ app/modules/admin/creator-revenues-controller.js
@@ -3,6 +3,8 @@ angular.module('webApp')
   function($scope, initializer, blogStub, errorFacade) {
     'use strict';
 
+    var fifthweekUserId = 'AAAAAAAAAAAAAAAAAAAAAA2';
+
     var model = $scope.model = {
       errorMessage: undefined,
       isLoading: false,
@@ -20,7 +22,20 @@ angular.module('webApp')
       model.errorMessage = undefined;
       return blogStub.getCreatorRevenues()
         .then(function(result){
-          model.creators = result.data.creators;
+
+          //var dummyResult = JSON.parse('{"creators":[{"userId":"AAAAAAAAAAAAAAAAAAAAAA2","unreleasedRevenue":21,"releasedRevenue":0,"releasableRevenue":21,"username":null,"name":null,"email":null,"emailConfirmed":false},{"userId":"-84gL0JjEeWT_gAVXcJ6PQ2","unreleasedRevenue":50,"releasedRevenue":0,"releasableRevenue":50,"username":"james2","name":"James Thurley","email":"james+james2@fifthweek.com","emailConfirmed":false}]}');
+          //var creators = dummyResult.creators;
+
+          var creators = result.data.creators;
+
+          _.forEach(creators, function(creator){
+            if(creator.userId === fifthweekUserId){
+              creator.username = 'Fifthweek';
+              creator.emailConfirmed = true;
+            }
+          });
+
+          model.creators = creators;
         })
         .catch(function(error){
           model.lookupResult = JSON.stringify(error, undefined, 2);
